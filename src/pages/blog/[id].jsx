4397643de960b0001e4d6c6b9ffbc1049a60a01f
Riku10145 <riku205riku@gmail.com>
2023-11-03 02:32:56 +0900

    ♻️ 拡張子をjsからjsxに変更

---
+++ src/pages/blog/[id].jsx
@@ -0,0 +1,43 @@
+import { Footer } from "../../../components/Footer";
+import { Header } from "../../../components/Header";
+import { client } from "../../../libs/client";
+
+
+export default function BlogId({ blog }) {
+  return (
+    <>
+    <Header />
+    <hr></hr>
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </main>
+    <hr></hr>
+    <Footer />
+    </>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+}
