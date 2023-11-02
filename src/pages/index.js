217e127813afbd25931d873ccb7dbbb8f1b8de36
Riku10145 <riku205riku@gmail.com>
2023-11-03 02:16:23 +0900

    🔥 不要なコードを削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import SearchBar from "../../components/Search";
-import { Header } from "../../components/Header";
-import styled from "@emotion/styled";
 
 const inter = Inter({ subsets: ["latin"] });
 
