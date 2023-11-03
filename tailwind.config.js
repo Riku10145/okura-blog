c0ab55cb7d2b5acd3e4211d0da7377ac6af758b4
Riku10145 <riku205riku@gmail.com>
2023-11-03 20:25:19 +0900

    🔧editor quick suggestionの 設定を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
