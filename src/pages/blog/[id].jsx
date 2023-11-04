235574bb06bdbed6e300537ae73f83c016dedc84
Riku10145 <riku205riku@gmail.com>
2023-11-04 13:22:48 +0900

    ♻️

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -7,14 +7,13 @@ export default function BlogId({ blog }) {
   return (
     <>
     <Header />
-    <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
-      <div
-        dangerouslySetInnerHTML={{
+    <main  className="mt-8 mx-auto max-w-prose h-screen">
+      <h1 className="text-xl font-bold mb-4">{blog.title}</h1>
+      <div dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
-      />
+        className="mb-4"/>
+        <p>{blog.publishedAt}</p>
     </main>
     <Footer />
     </>
